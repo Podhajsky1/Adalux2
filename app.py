9c8739ca580397e52574c62cea4442db82120249
Podhajsky1 <62499372+Podhajsky1@users.noreply.github.com>
2026-03-03 11:31:44 +0100

    Change default ANTHROPIC_KEY value
    Updated the default value for ANTHROPIC_KEY to a new API key.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
-ANTHROPIC_KEY = os.environ.get("ANTHROPIC_KEY", "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")
+ANTHROPIC_KEY = os.environ.get("ANTHROPIC_KEY", "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")
 ZNALOSTI_FILE = "adalux_znalosti.json"
 
 client = anthropic.Anthropic(api_key=ANTHROPIC_KEY)
